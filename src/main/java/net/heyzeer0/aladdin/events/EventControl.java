b06db771b534d250f438ef372e6d706544d51092
HeyZeer0 <heyzeer0@yahoo.com.br>
2017-10-08 13:31:30 -0300

    Fixed a massive glitch

--- src/main/java/net/heyzeer0/aladdin/events/EventControl.java
+++ src/main/java/net/heyzeer0/aladdin/events/EventControl.java
@@ -4,11 +4,16 @@ import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberLeaveEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.core.hooks.EventListener;
 import net.heyzeer0.aladdin.events.listeners.GuildListener;
 import net.heyzeer0.aladdin.events.listeners.MessageListener;
+import net.heyzeer0.aladdin.events.listeners.VoiceListener;
+import net.heyzeer0.aladdin.music.utils.AudioUtils;
 
 /**
  * Created by HeyZeer0 on 19/09/2017.
@@ -37,6 +42,29 @@ public class EventControl implements EventListener {
         if(e instanceof GuildMemberLeaveEvent) {
             GuildListener.onMemberLeave((GuildMemberLeaveEvent)e);
             return;
+        }
+        if(e instanceof GuildVoiceJoinEvent) {
+            GuildVoiceJoinEvent event = (GuildVoiceJoinEvent) e;
+            if(!event.getMember().getUser().isBot() && event.getChannelJoined().getMembers().contains(event.getGuild().getSelfMember())) {
+                VoiceListener.onVoiceJoin(((GuildVoiceJoinEvent)e).getChannelJoined());
+            }
+        }
+        if(e instanceof GuildVoiceLeaveEvent) {
+            GuildVoiceLeaveEvent event = (GuildVoiceLeaveEvent) e;
+            if(event.getChannelLeft().getMembers().contains(event.getGuild().getSelfMember()) && AudioUtils.isAlone(event.getChannelLeft())) {
+                VoiceListener.onVoiceLeave(((GuildVoiceLeaveEvent)e).getChannelLeft());
+            }
+
+        }
+        if(e instanceof GuildVoiceMoveEvent) {
+            GuildVoiceMoveEvent event = (GuildVoiceMoveEvent) e;
+            if(event.getChannelJoined().getMembers().contains(event.getGuild().getSelfMember()) && AudioUtils.isAlone(event.getChannelJoined())) {
+                VoiceListener.onVoiceJoin(event.getChannelJoined());
+            }
+
+            if(event.getChannelLeft().getMembers().contains(event.getGuild().getSelfMember()) && AudioUtils.isAlone(event.getChannelLeft())) {
+                VoiceListener.onVoiceLeave(event.getChannelLeft());
+            }
         }
     }
 
