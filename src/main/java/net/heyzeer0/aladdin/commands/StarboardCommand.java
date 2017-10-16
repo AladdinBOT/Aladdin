e106562867f9fe49c0427717e14d98738b8a30a0
HeyZeer0 <heyzeer0@yahoo.com.br>
2017-10-15 22:51:44 -0200

    Finished starboard command

--- src/main/java/net/heyzeer0/aladdin/commands/StarboardCommand.java
+++ src/main/java/net/heyzeer0/aladdin/commands/StarboardCommand.java
@@ -1,6 +1,7 @@
 package net.heyzeer0.aladdin.commands;
 
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.heyzeer0.aladdin.database.entities.StarboardProfile;
 import net.heyzeer0.aladdin.enums.CommandResultEnum;
 import net.heyzeer0.aladdin.enums.CommandType;
 import net.heyzeer0.aladdin.enums.EmojiList;
@@ -9,7 +10,10 @@ import net.heyzeer0.aladdin.interfaces.CommandExecutor;
 import net.heyzeer0.aladdin.profiles.commands.ArgumentProfile;
 import net.heyzeer0.aladdin.profiles.commands.CommandResult;
 import net.heyzeer0.aladdin.profiles.commands.MessageEvent;
+import net.heyzeer0.aladdin.profiles.utilities.Paginator;
 import net.heyzeer0.aladdin.profiles.utilities.Reactioner;
+
+import java.util.HashMap;
 
 /**
  * Created by HeyZeer0 on 12/06/2017.
@@ -52,7 +56,67 @@ public class StarboardCommand implements CommandExecutor {
 
             return new CommandResult(CommandResultEnum.SUCCESS);
         }
+
+        if(args.get(0).equalsIgnoreCase("remover")) {
+            if(args.getSize() < 2) {
+                return new CommandResult(CommandResultEnum.MISSING_ARGUMENT, "deletar", "id");
+            }
+
+            try{
+                Integer id = Integer.valueOf(args.get(1));
+                
+                if(id < 0) {
+                    e.sendMessage(EmojiList.WORRIED + " Oops, o número precisa ser maior ou igual a zero");
+                    return new CommandResult(CommandResultEnum.SUCCESS);
+                }
+
+                if(e.getGuildProfile().getStarboards().size() < id) {
+                    e.sendMessage(EmojiList.WORRIED + " Oops, a starboard com a id inserida não existe");
+                    return new CommandResult(CommandResultEnum.SUCCESS);
+                }
+
+                e.getGuildProfile().deleteStarboard(id);
+
+                e.sendMessage(EmojiList.CORRECT + " Você deletou com sucesso a starboard de id ``" + id + "``");
+
+            }catch (Exception ex) {
+                e.sendMessage(EmojiList.WORRIED + " Oops, a id inserida é invalida");
+            }
+
+            return new CommandResult(CommandResultEnum.SUCCESS);
+        }
+
         if(args.get(0).equalsIgnoreCase("list")) {
+
+            HashMap<String, StarboardProfile> starboards = e.getGuildProfile().getStarboards();
+
+            Paginator ph = new Paginator(e, ":newspaper: Listando todas as starboads!");
+
+            int pages = (starboards.size() + (10 + 1)) / 10;
+
+
+            Integer actual = 0;
+            Integer pactual = 1;
+
+
+            for (int i = 1; i <= pages; i++) {
+                String pg = "";
+                for (int p = actual; p < pactual * 10; p++) {
+                    if (starboards.size() <= p) {
+                        break;
+                    }
+                    actual++;
+
+                    String emote = starboards.get(p).getEmote().split("\\|")[0];
+
+                    pg = pg + "ID " + p + " | Emote: " +  emote + "(" + starboards.get(p).getMessages().size() + ")\n";
+                }
+                pactual++;
+                ph.addPage(pg);
+
+            }
+
+            ph.start();
             return new CommandResult(CommandResultEnum.SUCCESS);
         }
         return new CommandResult(CommandResultEnum.NOT_FOUND);
