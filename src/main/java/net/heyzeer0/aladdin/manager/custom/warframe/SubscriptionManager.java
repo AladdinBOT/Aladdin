423a122a4c25f02a1c6bb47d71c88616b2d90146
HeyZeer0 <heyzeer0@yahoo.com.br>
2018-02-16 19:07:47 -0200

    Added alert to Warframe cmd subscription

--- src/main/java/net/heyzeer0/aladdin/manager/custom/warframe/SubscriptionManager.java
+++ src/main/java/net/heyzeer0/aladdin/manager/custom/warframe/SubscriptionManager.java
@@ -6,12 +6,14 @@ import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.User;
 import net.heyzeer0.aladdin.Main;
+import net.heyzeer0.aladdin.profiles.custom.warframe.AlertProfile;
 import net.heyzeer0.aladdin.profiles.custom.warframe.SubscriptionProfile;
 import net.heyzeer0.aladdin.utils.Utils;
 import org.json.JSONObject;
 
 import java.awt.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -26,6 +28,7 @@ public class SubscriptionManager {
 
     public static HashMap<String, SubscriptionProfile> subscriptions = null;
     public static boolean acceptNight = true;
+    public static ArrayList<String> sendedAlerts = new ArrayList<>();
 
     private static ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
 
@@ -53,6 +56,15 @@ public class SubscriptionManager {
 
                 try{
                     JSONObject cycle = new JSONObject(Utils.readWebsite("https://api.warframestat.us/pc/cetusCycle"));
+                    List<AlertProfile> alerts = AlertManager.getAlerts();
+                    List<AlertProfile> selectedAlerts = new ArrayList<>();
+
+                    for(AlertProfile alert : alerts) {
+                        if(!sendedAlerts.contains(alert) && alert.hasLoot() && alert.getRewordID().getName().toLowerCase().contains("orokin")) {
+                            selectedAlerts.add(alert);
+                            sendedAlerts.add(alert.getId());
+                        }
+                    }
 
                     if(!cycle.getBoolean("isDay")) {
                         if(acceptNight) {
@@ -74,14 +86,46 @@ public class SubscriptionManager {
 
                             if(m.getOnlineStatus() == OnlineStatus.ONLINE) {
 
-                                if(sendNight || u.getId().equalsIgnoreCase("169904764048375809")) {
+                                if(sendNight) {
                                     EmbedBuilder b = new EmbedBuilder();
                                     b.setTitle("<:level:363725048881610753> Status das planícies");
-                                    b.setDescription("É noite nas planícies!\nTempo restante: ``" + cycle.getString("timeLeft") + "``");
+                                    b.setDescription("É noite nas planícies!\nTempo restante ``" + cycle.getString("timeLeft") + "``");
                                     b.setFooter("Requerido por " + u.getName(), u.getEffectiveAvatarUrl());
                                     b.setTimestamp(LocalDateTime.now());
                                     b.setColor(Color.GREEN);
 
+                                    u.openPrivateChannel().queue((success) -> success.sendMessage(b.build()).queue(), (failure) -> subscriptions.remove(userId));
+                                }
+
+                                if(selectedAlerts.size() > 0) {
+
+                                    EmbedBuilder b = new EmbedBuilder();
+                                    b.setColor(Color.CYAN);
+
+                                    if(selectedAlerts.size() >= 2) {
+                                        b.setTitle("Alertas raros disponíveis", null);
+                                    }else{
+                                        b.setTitle("Alerta raro disponível", null);
+                                    }
+                                    b.setThumbnail("http://vignette4.wikia.nocookie.net/warframe/images/c/ce/OrdisArchwingtrailer.png/revision/latest?cb=20140823050147");
+                                    b.setDescription("Listando todos os alertas raros disponíveis");
+
+                                    Integer alertas = 0;
+                                    for (AlertProfile p : selectedAlerts) {
+                                        alertas++;
+                                        b.addField("<:lotus:363726000871309312> Alerta " + alertas + " | :clock1: " + p.getTimeLeft() + " restantes",
+                                                "<:liset:363725081404375040> Localidade: " + p.getLocation() + " | " + p.getMission().getMission() + " | " + p.getMission().getFaction() + "\n" + (p.hasLoot() ?
+                                                        "<:mod:363725102472495107> Recompensa: [" + p.getRewordID().getName() + "](" + p.getRewordID().getDirectURL() + ")" + "\n" : "") +
+                                                        "<:credits:363725076845035541> Créditos: " + p.getCredits() + "\n" +
+                                                        "<:level:363725048881610753> Nível mínimo: " + p.getMinLevel()
+                                                , false);
+                                    }
+
+                                    if (alertas == 0) {
+                                        b.addField("", "Não há alertas disponíveis", true);
+                                    }
+
+                                    b.setFooter("Warframe Status", "http://img05.deviantart.net/b8d4/i/2014/327/a/8/warframe_new_logo_look__vector__by_tasquick-d87fzxg.png");
                                     u.openPrivateChannel().queue((success) -> success.sendMessage(b.build()).queue(), (failure) -> subscriptions.remove(userId));
                                 }
                             }
