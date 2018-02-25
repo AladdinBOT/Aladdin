bb49745cfc0fbd7e0fc0771638eda34a91d6d29a
HeyZeer0 <heyzeer0@yahoo.com.br>
2018-02-24 21:46:06 -0300

    Amazing

--- src/main/java/net/heyzeer0/aladdin/commands/BotCommand.java
+++ src/main/java/net/heyzeer0/aladdin/commands/BotCommand.java
@@ -88,7 +88,7 @@ public class BotCommand implements CommandExecutor {
             b.setColor(Color.GREEN);
             b.setAuthor("Sobre Aladdin", null, "https://media.tenor.com/images/9a5178a7b636e201da025b7e41f8e2a2/tenor.gif");
             b.setDescription("Olá meu nome é Aladdin, caso você não saiba eu fui inspirado no anime [Magi](https://pt.wikipedia.org/wiki/Magi_(mang%C3%A1), eu tenho como foco ações administrativas porém posso ser bastante útil em outras coisas variantes desde diversão até musica, sendo desenvolvido pelo ``HeyZeer0#0190``. \nEm minha aventura eu sou acompanhado pelos meus grandes amigos Ali babá e Morgiana, talvez você encontre eles por aí. Meu objetivo é aprender mais e mais sobre este mundo, você pode me chamar para sua guilda clicando [aqui](https://discordapp.com/oauth2/authorize?client_id=321349548712656896&scope=bot&permissions=2146958463)!\nEae, gostaria de ser meu amigo?");
-            b.setImage("http://dl.heyzeer0.cf/Aladdin/aladdin_friend.png");
+            b.setImage("http://dl.heyzeer0.tk/Aladdin/aladdin_friend.png");
             b.setFooter("Aladdin v" + Main.version, e.getJDA().getSelfUser().getAvatarUrl());
             e.sendMessage(b);
             return new CommandResult(CommandResultEnum.SUCCESS);
