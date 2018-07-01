a92c3f4b9b123b001be4e76ceb73f86018814368
HeyZeer0 <heyzeer0@yahoo.com.br>
2018-07-01 16:04:00 -0300

    Holy cow

--- src/main/java/net/heyzeer0/aladdin/events/listeners/GuildListener.java
+++ src/main/java/net/heyzeer0/aladdin/events/listeners/GuildListener.java
@@ -25,7 +25,7 @@ public class GuildListener {
         }
 
         if(e.getGuild().getSelfMember().hasPermission(e.getGuild().getDefaultChannel(), Permission.MESSAGE_WRITE)) {
-            e.getGuild().getDefaultChannel().sendMessage(pf.getSelectedLanguage().getLangProfile().get("guild.join.message"));
+            e.getGuild().getDefaultChannel().sendMessage(pf.getSelectedLanguage().getLangProfile().get("guild.join.message")).queue();
         }
 
         Main.getLogger().embed(":house: Guild Join Event", "Guilda ``" + e.getGuild().getName() + "``\nUsers ``" + e.getGuild().getMembers().size() + "``\nDono ``" + e.getGuild().getOwner().getUser().getName() + "#" + e.getGuild().getOwner().getUser().getDiscriminator() + "``", Color.GREEN);
